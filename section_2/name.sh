80267853f7e6e6cde0c931a64eac08c4f45d797a
jskim7827 <52936426+jskim7827@users.noreply.github.com>
2019-08-16 10:46:53 -0400

    Update name.sh

--- section_2/name.sh
+++ section_2/name.sh
@@ -2,10 +2,3 @@
 
 # echo "Hello ${name}"
 
-# curl https://s3.amazonaws.com//aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O
-#chmod +x ./awslogs-agent-setup.py
-#./awslogs-agent-setup.py -n -r us-east-1 -c s3://<your-public-bucket-naame>/my-config-file
-
-
-curl -OLs https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb 
-dpkg -i -E ./amazon-cloudwatch-agent.deb
